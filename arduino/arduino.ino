232616feb9e31ce00d18bfa20318f44bd6609f0c
Raúl González <raulzc3pc@gmail.com>
2023-01-19 23:50:20 +0100

    Upgrade

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -12,6 +12,7 @@ int timeLimit = defaultTimeLimit;
 int timer = 0;
 
 
+
 WiFiServer server(80);
 
 
@@ -74,12 +75,16 @@ void setup() {
 
 void loop() {
  
-  
+ // Serial.print("Free Heap: ");
+ // Serial.println(ESP.getFreeHeap());
+ // Serial.print("Fragmentation: ");
+ // Serial.println(ESP.getHeapFragmentation());
+
   // Check for an active client
   WiFiClient client = server.available();
   if (!client) {
     if(relayStatus == HIGH){
-      delay(1000);
+      delay(950);
       timer++;
       Serial.println(timeLimit);
       if(timer == timeLimit){
@@ -129,18 +134,15 @@ void loop() {
   }
   
  
-String html ="<!DOCTYPE html> <html lang='es'> <head> <meta charset='UTF-8'> <meta name='viewport' content='width=device-width, initial-scale=1.0'> <title>Calefacción</title> <style> * { box-sizing: border-box; margin: 0; padding: 0; font-family: Arial, Helvetica, sans-serif; color: white; text-align: center; } main { width: 100vw; height: 100vh; margin: 0; padding: 0; border: 2rem solid darkgray; display: flex; align-items: center; justify-content: center; gap: 1.5rem; flex-direction: column; background-color: rgb(37, 37, 37); font-size: 2rem; } #status { font-size: 3rem; } #statusSpan { font-size: .7em; text-align: center; margin-top: -2rem; } #actions { display: flex; flex-direction: column; gap: .5rem; } #actions span { margin-bottom: 1rem; margin-top: 1rem; } a { text-decoration: none; padding: .5rem; border: 10px solid white; transition: .3s; } a:hover { background-color: white; color: rgb(37, 37, 37); border: 10px solid black; } .on { border-image: linear-gradient(white, yellow, orange, rgb(255, 72, 0), red) 3; } </style> </head> <body> <main id=\"main\"> <span id=\"statusSpan\">Calefacción <br /> <b id='status'>Apagada</b> </span> </p> <span id=\"info\"> Encender: </span> <div id=\"actions\"> <span> <a href='/ON/timer=00900'>15min</a> <a href='/ON/timer=01800'>30min</a> </span> <a href='/ON/timer=03600'>1h</a> <a href='/ON/timer=05400'>1h 30min</a> <a href='/ON/timer=07200'>2h</a> </div> </main> <script> const relayStatus = ";
-html += relayStatus;
-html +="; let timeRemaining = ";
-html += timeLimit - timer;
-html +="; const info = document.getElementById(\"info\"); const status = document.getElementById('status'); const actionContainer = document.querySelector('#actions'); function setTime(timer) { let acc = timer; let hours = Math.floor(timer / 3600).toString().padStart(2, \"0\"); acc = acc - hours * 3600; let minutes = Math.floor(acc / 60).toString().padStart(2, \"0\"); acc = acc - minutes * 60; let seconds = acc.toString().padStart(2, \"0\"); return `${hours}:${minutes}:${seconds}`; } if (relayStatus) { const interval = setInterval(() => { if (timeRemaining > 0) { timeRemaining--; document.getElementById(\"time\").innerText = setTime(timeRemaining); } else {document.location.href = '/'; clearInterval(interval); } }, 1000); status.innerText = 'Encendida'; info.innerHTML = `Se apagará en: <br> <b id=\"time\">${setTime(timeRemaining)}</b>`; document.getElementById('main').classList.add('on'); actionContainer.innerHTML = \"<a href='/OFF'>Apagar</a>\"; } </script> </body> </html>";
-
-
-
-client.print(html);
+client.print("<!DOCTYPE html> <html lang='es'> <head> <meta charset='UTF-8'> <meta name='viewport' content='width=device-width, initial-scale=1.0'> <title>Calefacción</title> <style> * { box-sizing: border-box; margin: 0; padding: 0; font-family: Arial, Helvetica, sans-serif; color: white; text-align: center; } main { width: 100vw; height: 100vh; margin: 0; padding: 0; border: 2rem solid darkgray; display: flex; align-items: center; justify-content: center; gap: 1.5rem; flex-direction: column; background-color: rgb(37, 37, 37); font-size: 2rem; } #status { font-size: 3rem; } #statusSpan { font-size: .7em; text-align: center; margin-top: -2rem; } #actions { display: flex; flex-direction: column; gap: .5rem; } #actions span { margin-bottom: 1rem; margin-top: 1rem; } a { text-decoration: none; padding: .5rem; border: 10px solid white; transition: .3s; } a:hover { background-color: white; color: rgb(37, 37, 37); border: 10px solid black; } .on { border-image: linear-gradient(white, yellow, orange, rgb(255, 72, 0), red) 3; } </style> </head> <body> <main id=\"main\"> <span id=\"statusSpan\">Calefacción <br /> <b id='status'>Apagada</b> </span> </p> <span id=\"info\"> Encender: </span> <div id=\"actions\"> <span> <a href='/ON/timer=00900'>15min</a> <a href='/ON/timer=01800'>30min</a> </span> <a href='/ON/timer=03600'>1h</a> <a href='/ON/timer=05400'>1h 30min</a> <a href='/ON/timer=07200'>2h</a> </div> </main> <script>if(window.location.pathname !== '/'){window.location.replace('http://192.168.1.100/')} const relayStatus = ");
+client.print(relayStatus);
+client.print("; let timeRemaining = ");
+client.print(timeLimit - timer);
+client.print("; const info = document.getElementById(\"info\"); const status = document.getElementById('status'); const actionContainer = document.querySelector('#actions'); function setTime(timer) { let acc = timer; let hours = Math.floor(timer / 3600).toString().padStart(2, \"0\"); acc = acc - hours * 3600; let minutes = Math.floor(acc / 60).toString().padStart(2, \"0\"); acc = acc - minutes * 60; let seconds = acc.toString().padStart(2, \"0\"); return `${hours}:${minutes}:${seconds}`; } if (relayStatus) { const interval = setInterval(() => { if (timeRemaining > 0) { timeRemaining--; document.getElementById(\"time\").innerText = setTime(timeRemaining); } else {location.reload(); clearInterval(interval); } }, 1000); status.innerText = 'Encendida'; info.innerHTML = `Se apagará en: <br> <b id=\"time\">${setTime(timeRemaining)}</b>`; document.getElementById('main').classList.add('on'); actionContainer.innerHTML = \"<a href='/OFF'>Apagar</a>\"; } </script> </body> </html>");
  
 
-  delay(1);
+  delay(50);
+  
   Serial.println("Client disconnected");
   Serial.println("");
 
